3ba8f58f2e3304191ac86866b8a474d79098a56f
Iqbal Hasan <iqbalhasan6116@gmail.com>
2023-11-27 19:45:44 +0600

    Constants

---
+++ 05Constants.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    // constant = fixed calue that cannot be altered by the program during its execution
+
+    const float PI = 3.1416;
+
+    printf("%f",PI);
+
+
+
+    printf("\n");
+    return 0;
+}
