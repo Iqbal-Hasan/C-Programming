533a92b6407989d506f3e9dc3330930284306674
Iqbal Hasan <iqbalhasan6116@gmail.com>
2023-12-14 13:34:12 +0600

    Math function

--- 08MathFunction.c
+++ 08MathFunction.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
-    printf("hello");
+
+    double A = sqrt(9);
+    double B = pow(2 ,4);
+    int C = round(3.22);
+    int C2 = round(3.52);
+    int D = ceil(3.14);
+    int E = floor(3.14);
+    double F = fabs(-100);
+    double G = log(3);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
+
+    printf("\n%lf",H);
+    printf("\n%lf",I);
+    printf("\n%lf",J);
     printf("\n");
     return 0;
 }
