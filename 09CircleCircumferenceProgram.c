78efa7d07217ce497210f27709f5adaaaa0cb02e
Iqbal Hasan <iqbalhasan6116@gmail.com>
2023-12-18 10:17:42 +0600

    circle

--- 09CircleCircumferenceProgram.c
+++ 09CircleCircumferenceProgram.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-int main(){
-
-    // circumference and area of circle
+int main()
+{
+    // circumference and area of circle random random
     const double PI = 3.14159;
     double radius;
     double circumference;
     double area;
 
     printf("Enter your circle radius : ");
-    scanf("%lf",&radius);
+    scanf("%lf", &radius);
 
     circumference = 2 * PI * radius;
     area = PI * radius * radius;
 
-    printf("Circumference : %lf\n",circumference);
-    printf("Area : %lf", area);
+    printf("Circumference : %lf\n", circumference);
 
     printf("\n");
     return 0;
